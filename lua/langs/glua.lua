e8563c49a89000e5365c3c58c3aec01713e7d49e
Pierce <pierce@vulpes.dev>
2023-04-19 22:50:53 -0400

    update go and glua language server configs

--- lua/langs/glua.lua
+++ lua/langs/glua.lua
@@ -8,6 +8,19 @@ lsp.configure('lua_ls', {
                     "/home/pierce/LuaAddons/glua/"
                 }
             },
+            format = {
+                enable = true,
+                defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "4",
+                    tab_width = "4",
+                    quote_style = "double",
+                    max_line_length = "120",
+                    insert_final_newline = "true",
+                    space_inside_function_call_parentheses = "true",
+                    space_inside_function_param_list_parentheses = "true",
+                },
+            },
             runtime = {
                 special = {
                     include = "require"
--- lua/langs/go.lua
+++ lua/langs/go.lua
@@ -1,3 +1,36 @@
+local lsp = require("lsp-zero")
+
+lsp.configure('gopls', {
+    settings = {
+        gopls = {
+            staticcheck = true,
+            codelenses = {
+                gc_details = true,
+            },
+            analyses = {
+                -- ST1003 = true, -- poorly chosen identifier
+                ST1005 = true,
+                ST1006 = true,
+                ST1013 = true,
+                ST1018 = true,
+                ST1020 = true,
+                ST1021 = true,
+                ST1022 = true,
+                ST1023 = true,
+                ST1016 = true,
+            }
+        }
+    }
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+    end
+})
+
+
 local null_ls = require("null-ls")
 
 null_ls.setup({
